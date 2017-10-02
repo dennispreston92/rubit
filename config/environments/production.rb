b080c6b943439aae285d219be5be7f123b5ef8fb
dennispreston92 <dennispreston@hotmail.co.uk>
2017-10-02 15:11:24 +0100

    add mailer sender address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://rubit.org.uk/' }
+  config.action_mailer.default_url_options = { host: 'http://rubit.org.uk/' }
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
