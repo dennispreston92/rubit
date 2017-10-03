6c515d9b60efe0320add210695712f3bfd777659
dennispreston92 <dennispreston@hotmail.co.uk>
2017-10-03 09:33:55 +0100

    change env variable s3 bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
       region:                'eu-west-2', # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = ENV['S3_BUCKET'] # required
+    config.fog_directory  = ENV['S3_BUCKET_NAME'] # required
     config.fog_public     = true # optional, defaults to true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   elsif Rails.env.development?
