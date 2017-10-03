2077b11cf067c4069bd0c06b6106c7ef3144855b
dennispreston92 <dennispreston@hotmail.co.uk>
2017-10-03 21:25:55 +0100

    add tooltips

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,5 @@
 $(document).ready(function () {
   $("#event_event_datetime").datetimepicker({format: 'Y/m/d H:i'});
   $("#user_dob").datetimepicker({timepicker: false, format: 'Y/m/d', maxDate: '0'});
+  $('[data-toggle="tooltip"]').tooltip();
 });
